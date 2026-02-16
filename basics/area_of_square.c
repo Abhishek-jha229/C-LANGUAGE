3201339940b6c4b4440af65bb451b164b7006931
Abhishek-jha229 <jha83583@gmail.com>
2026-02-16 23:05:01 +0530

    Updated area_of_square program

--- basics/area_of_square.c
+++ basics/area_of_square.c
@@ -4,6 +4,6 @@ int main()
     int side;
     printf("enter side =");
     scanf("%d",&side);
-    printf("Area of Circle is %d", side*side);
+    printf("Area of square is %d", side*side);
     return 0;
 }
