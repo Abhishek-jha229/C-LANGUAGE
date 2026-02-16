1f0ce58a3777d2ba0f22759e73142729bf1c2961
Abhishek-jha229 <jha83583@gmail.com>
2026-02-16 23:07:39 +0530

    Added Average_of_3numbers program

---
+++ basics/Average_of_3numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,avg;
+    printf("enter a");
+    scanf("%f",&a);
+     printf("enter b");
+    scanf("%f",&b);
+    printf("enter c");
+    scanf("%f",&c);
+    avg=(a+b+c)/3;
+    printf("%f",avg);
+    return 0;
+
+   
+}
