44f0fa942cb526cc624109f350dc62133c42f396
Abhishek-jha229 <jha83583@gmail.com>
2026-02-22 08:22:26 +0530

    Added a program to check_pass_fail

---
+++ basics/Check_pass_fail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("enter student's marks");
+    scanf("%d",&marks);
+    if(marks<=33){
+        printf("FAIL");
+    }else{
+        printf("PASS");
+    }
+    }
