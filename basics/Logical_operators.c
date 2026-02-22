f1948baae851d83d152dccfb856397621bd805dc
Abhishek-jha229 <jha83583@gmail.com>
2026-02-22 08:14:36 +0530

    Added a program of Logical_operators

---
+++ basics/Logical_operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int issunday =1;
+    int ismonday =1;
+    printf("%d\n", issunday&&ismonday);
+    printf("%d\n", issunday|| ismonday);
+    printf("%d\n", !issunday);
+    printf("%d\n", issunday != ismonday);
+    return 0;
+}
