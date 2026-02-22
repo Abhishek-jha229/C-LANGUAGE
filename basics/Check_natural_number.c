c807ee8135bc3f3a1dbd84a49d499415a820c7bb
Abhishek-jha229 <jha83583@gmail.com>
2026-02-22 08:17:50 +0530

    Added a program to  check_natural_number

---
+++ basics/Check_natural_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+
+    if(n>=1){
+        printf("number is natural ");
+    }else{
+        printf("not a natural number");
+    }
+    return 0;
+
+}
