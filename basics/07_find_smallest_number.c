76c73a95892bb927b929319f3ae5bed400daca9c
Abhishek-jha229 <jha83583@gmail.com>
2026-02-22 08:05:14 +0530

    Added a c program to find_smallest_number

---
+++ basics/07_find_smallest_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a<b && a<c){
+        printf("smallest number is %d",a);
+    }
+    if(b<c && b<a){
+        printf("smallest number is %d",b);
+    }
+    if(c<a && c<b){
+        printf("smallest number is %d",c);
+    }
+    return 0;
+}
